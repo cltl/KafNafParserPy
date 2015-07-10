781d523bcefe2547cfcb618380598f1feac4e873
Antske <antske@gmail.com>
2015-07-10 14:17:52 +0200

    Added import to external references

--- coreference_data.py
+++ coreference_data.py
@@ -3,6 +3,7 @@ This module implements a parser for the coreference layer in KAF/NAF
 """
 
 from lxml import etree
+from external_references_data import *
 from span_data import Cspan
 
 class Ccoreference:
