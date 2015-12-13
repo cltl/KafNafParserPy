7314d05f03499e1859a916c19748a06dbd00189c
Antske <antske@gmail.com>
2015-12-13 13:47:00 +0100

    Added function that gives full dependency graph from given term

--- feature_extractor/dependency.py
+++ feature_extractor/dependency.py
@@ -351,6 +351,23 @@ class Cdependency_extractor:
                 shortest_path = this_path
         return shortest_path
         
+    # Get all terms that are embedded under a given head (its dependents and dependents of its dependents
+    def get_full_dependents(self, term_id, relations):
+        """
+        Returns the complete list of dependents and embedded dependents of a certain term.
+        """
+        relations = []
+        counter += 1
+        deps = self.relations_for_term
+        if term_id in deps and len(deps.get(term_id)) > 0:
+            for dep in deps.get(term_id):
+                if not dep[1] in relations:
+                    relations.append(dep[1])
+                    if dep[1] in deps:
+                        dep_relations = self.get_full_dependents(dep[1], relations)
+                        relations += dep_relations
+        return relations
+
 
 
                 
