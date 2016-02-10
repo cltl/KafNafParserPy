58d081ea7e56984fec7c3827f2d20e7900ea69ad
Ruben Izquierdo <r.izquierdobevia@vu.nl>
2016-02-10 11:13:20 +0100

    Added setup.py, Added to python package index (pip)

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+try:
+      from setuptools import setup
+except ImportError:
+      from distutils.core import setup
+
+
+
+
+
+setup(name='KafNafParserPy',
+      version='1.6',
+      description='Parser for KAF/NAF files in Python',
+      author='Ruben Izquierdo',
+      author_email='rubensanvi@gmail.com',
+      url='https://github.com/cltl/KafNafParserPy',
+      packages = ['KafNafParserPy','KafNafParserPy.feature_extractor'],
+      package_dir = {'KafNafParserPy':'.'},
+      install_requires = ['lxml']
+      )
